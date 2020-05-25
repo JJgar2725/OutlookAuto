79593a0c7a672c961335bc38baf5753362041829
Jaime Garcia Jr <jjgarcia2725@gmail.com>
2020-05-24 23:54:50 -0500

    testing commit

--- outlook.py
+++ outlook.py
@@ -14,10 +14,15 @@ username.send_keys(os.environ['USER-NAME'])
 password = driver.find_element_by_id("_58_password")
 password.send_keys(os.environ['PASSWORD'])
 
-submit_button = driver.find_element_by_xpath('//*[@id="_58_fm"]/div/button').click()
+submit_button = driver.find_element_by_xpath(
+    '//*[@id="_58_fm"]/div/button').click()
 
 driver.implicitly_wait(10)  # FIXME: USE EXPLICIT WAITING
-outlook_select = driver.find_element_by_xpath('//*[@id="portlet_utrgvgraphemailportlet_WAR_utrgvgraphemailportlet"]/div/div/div/div/article[1]/a').click()
+outlook_select = driver.find_element_by_xpath(
+    '//*[@id="portlet_utrgvgraphemailportlet_WAR_utrgvgraphemailportlet"]/div/div/div/div/article[1]/a').click()
 
 driver.implicitly_wait(20)  # FIXME: USE EXPLICIT WAITING
-new_message = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[1]/div/div[1]/div[1]/div[2]/button').click()
+new_message = driver.find_element_by_xpath(
+    '//*[@id="app"]/div/div[2]/div[1]/div/div[1]/div[1]/div[2]/button').click()
+
+testing = driver.get("https://google.com")
